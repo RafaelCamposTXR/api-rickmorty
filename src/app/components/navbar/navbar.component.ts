8b836a494f4f359db768bbfb3cd1cb89993ad34a
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-11-03 23:04:17 -0300

    integrando navbar com botão de navegação da tela de favoritos

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Store, Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { FavoritesState, Character } from '../../store/favorites.state';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-navbar',
@@ -12,16 +16,23 @@ export class NavbarComponent implements OnInit {
   @Select(FavoritesState.getFavorites) favorites$!: Observable<Character[]>;
 
   favoriteCount: number = 0;
+  activeRoute: string = ''; 
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private router: Router) {}
 
   ngOnInit(): void {
     this.favorites$.subscribe(favorites => {
       this.favoriteCount = favorites.length;
     });
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd)) // Filtra os eventos do tipo NavigationEnd
+    .subscribe((event) => {
+      const navigationEndEvent = event as NavigationEnd; // Fazendo cast para NavigationEnd
+      this.activeRoute = navigationEndEvent.urlAfterRedirects; // Atualiza a rota ativa
+    });
   }
-
   menuOpen = false;
+
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
